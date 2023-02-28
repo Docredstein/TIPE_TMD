795377b228f736d3b70d4cea1d2e56d99de410a6
Docredstone <francais.minecraftien@laposte.net>
2023-02-28 23:35:41 +0100

    PID complete

--- Firmware_CI/fw/PID.cpp
+++ Firmware_CI/fw/PID.cpp
@@ -1,1 +1,28 @@
 #include "PID.h"
+#include "hardware/timer.h"
+PID::PID(float Kp,float Ki,float Kd, float max_derivative, float max_integral) {
+    m_Kp = Kp;
+    m_Ki = Ki;
+    m_Kd = Kd;
+    m_max_d = max_derivative;
+    m_max_int = max_integral; 
+    m_integral = 0; 
+    m_lastError = 0;
+    m_lastMicros = time_us_32();
+    m_consigne = 0;
+}
+int PID::update_consigne(float consigne) {
+    m_consigne = consigne;
+    return 0;
+    
+}
+float PID::update_measure(float measure) {
+    auto current_micros = time_us_32();
+    float dt = (current_micros - m_lastMicros)/1e6; 
+    float error = (measure-m_consigne);
+    float derivative = MIN((error-m_lastError)/dt,m_max_d);
+    m_integral = MIN(m_integral + error*dt,m_max_int);
+    m_lastError = error;
+    m_lastMicros = current_micros;
+    return m_Kp*error+m_Ki*m_integral+m_Kd*derivative;
+}
--- Firmware_CI/fw/PID.h
+++ Firmware_CI/fw/PID.h
@@ -2,6 +2,8 @@
 #define PID_H 
 
 #include <stdio.h>
+#include <stdint.h>
+
 class PID
 {
 private:
@@ -9,12 +11,14 @@ private:
     float m_Kp,m_Ki,m_Kd,m_max_d,m_max_int;
     float m_lastError; 
     float m_integral;
+    float m_consigne;
+    uint32_t  m_lastMicros;
 public:
     PID(float Kp,float Ki,float Kd, float max_derivative = 1000, float max_integral = 1000);
     ~PID();
-    int update_measure(int mesure);
-    int update_consigne(int consigne);
-    float get_command();
+    float update_measure(float mesure);
+    int update_consigne(float consigne);
+    //float get_command();
 };
 
 
