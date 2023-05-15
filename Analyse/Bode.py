0ef5c9b3d8b35195d737aea97e5eea86dd9d0b41
Docredstone <francais.minecraftien@laposte.net>
2023-05-15 18:06:23 +0200

    Mesure très faible amplitude

--- Analyse/Bode.py
+++ Analyse/Bode.py
@@ -28,9 +28,9 @@ def clean() :
             mean = np.mean(frame[:,i])
             for j in frame[:,i] :
                 if abs(j-mean)>2*std :
-                    out.append(mean)
+                    out.append(0)
                 else :
-                    out.append(j)
+                    out.append(j-mean)
             mat.append(out)
         mat = np.transpose(mat) 
         #print(mat)
@@ -45,10 +45,10 @@ def clean() :
         plt.xlabel("t (s)")
         plt.ylabel("a (mg)")
         try :
-            plt.savefig("Image/"+folder+name.replace("csv","pdf"))
+            plt.savefig("Image/"+folder+name.replace(".csv",".pdf"))
         except FileNotFoundError:
             os.mkdir("Image/"+folder)
-            plt.savefig("Image/"+folder+name.replace("csv","pdf"))
+            plt.savefig("Image/"+folder+name.replace(".csv",".pdf"))
         try :
             np.savetxt("Cleaned/"+folder+name,mat,delimiter=";")
         except FileNotFoundError:
@@ -106,20 +106,65 @@ def bode()  :
     np.savetxt(folder.split("/")[-2]+".csv",out,delimiter=";")
     #plt.show()
     return (freq, gain, phase)
-for folder in tqdm(os.listdir("ToClean/")) :
-    print(f"\033[92m Treating folder :\033[0m {folder}")
-    #folder = "masse en haut faible amplitude/"
-    try : 
-        folder = folder + "/"
-        liste_fichier  = os.listdir("ToClean/"+folder+"/")
-        clean()
-        print(f"\033[92mfolder Cleaned, creating bode diagramm\033[0m")
-    except :
-        print("\033[91mERROR\033[0m")
-        raise
-    bode()
-    print(f"\033[92mfolder completely treated\033[0m")
-print(f"\033[92m\033[4mfolder all done")
+def bode_norme()  : 
+    gain = []
+    phase = []
+    freq = []
+    plt.figure(figsize=(10,5))
+    for i,name in enumerate(liste_fichier) :
+        frame = np.genfromtxt("Cleaned/"+folder+name,delimiter=";")
+        #print(len(frame))
+        bas = np.sqrt(np.square(frame[:,1])+np.square(frame[:,2]))
+        haut = np.sqrt(np.square(frame[:,4])+np.square(frame[:,5]))
+        bas = bas - np.mean(bas) 
+        haut = haut - np.mean(haut) 
+        fft_bas = fft(bas)
+        fft_haut = fft(haut)
+        n = np.arange(len(fft_bas))
+        
+        frequence = (n/(frame[-1,0]-frame[0,0]))*1e6 #f = n/deltaT
+        #print(n)
+        #print(frequence)
+        f_ind = np.argmax(np.abs(fft_bas)[:end_freq(frequence,10)]) 
+        
+        #print(f_ind)
+        freq.append(frequence[f_ind])
+        gain.append(20*np.log(np.abs(fft_haut[f_ind])/np.abs(fft_bas[f_ind]))) 
+        phase.append(np.angle(fft_haut[f_ind])-np.angle(fft_bas[f_ind]))
+        #print(name,freq[-1],gain[-1],phase[-1])
+    plt.subplot(2,1,1)
+    plt.semilogx(freq,gain,"x")
+    plt.grid(True,"both")
+    plt.ylabel("$G_{dB}$ (dB)")
+    plt.subplot(2,1,2) 
+    plt.semilogx(freq,phase,"x")
+    plt.ylabel("φ (rad)")
+    plt.xlabel("Fréquence d'excitation (Hz)")
+    plt.grid(True,"both")
+    plt.suptitle(folder.split("/")[-2]+"en norme")
+    plt.savefig("Image/"+folder.replace(" ","_").split("/")[-2]+"_norme"+".pdf")
+    out = np.concatenate((freq,gain,phase)).reshape((len(freq),3))
+    np.savetxt(folder.split("/")[-2]+"_norme"+".csv",out,delimiter=";")
+    #plt.show()
+    return (freq, gain, phase)
+
+
+if __name__ == "__main__" :
+    for folder in tqdm(os.listdir("ToClean/")) :
+        print(f"\033[92m Treating folder :\033[0m {folder}")
+        #folder = "masse en haut faible amplitude/"
+        try : 
+            folder = folder + "/"
+            liste_fichier  = os.listdir("ToClean/"+folder+"/")
+            clean()
+            print(f"\033[92mfolder Cleaned, creating bode diagramm\033[0m")
+        except :
+            print("\033[91mERROR\033[0m")
+            raise
+        bode()
+        #bode_norme() #Ne fonctionne pas ==> ce n'est pas linéaire
+        print(f"\033[92mfolder completely treated\033[0m")
+    print(f"\033[92m\033[4mfolder all done")
     
 
 
